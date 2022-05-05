824928d4501e1f96bdf2d707aa7d060eeacb13db
NosovaDaria <kuleshovadasha@gmail.com>
2022-05-05 15:41:02 +0300

    1. Setup CurrentWeatherCell 2. Setup Bundle 3. Setup ModelItem 3. Configured WeatherViewController 4. Configured ModelWeather

---
+++ Weather/Controller/CurrentWeatherCell.swift
@@ -0,0 +1,103 @@
+//
+//  CurrentWeatherCell.swift
+//  Weather
+//
+//  Created by Дарья Носова on 04.05.2022.
+//
+
+import Foundation
+import UIKit
+
+class CurrentWeatherCell: UICollectionViewCell {
+    static var reuseId: String = "CurrentWeatherCell"
+    
+    let temperature = UILabel()
+    
+    let weatherIcon = UIImageView()
+    let weatherDescription = UILabel()
+    let feelsLike = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor(white: 1, alpha: 1)
+        
+        customizeElements()
+        setupConstraints()
+    }
+    
+    func configure(with modelWeather: ModelWeather) {
+        let weather = modelWeather.current
+        temperature.text = "+\(Int(round(weather.temp * 10)/10))°"
+        weatherIcon.image = UIImage(named: weather.weather[0].icon)
+        weatherDescription.text = weather.weather[0].description ?? ""
+        feelsLike.text = "Ощущается как +\(Int(round(weather.feels_like * 10)/10))°"
+    }
+    
+    func customizeElements() {
+        temperature.textColor = .black
+        temperature.font = UIFont(name: "avenir", size: 35)
+        temperature.translatesAutoresizingMaskIntoConstraints = false
+        
+        weatherIcon.translatesAutoresizingMaskIntoConstraints = false
+        
+        weatherDescription.textColor = .black
+        weatherDescription.font = UIFont(name: "avenir", size: 16)
+        weatherDescription.translatesAutoresizingMaskIntoConstraints = false
+        
+        feelsLike.textColor = .black
+        feelsLike.font = UIFont(name: "avenir", size: 16)
+        feelsLike.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+// MARK: - Setup Constraints
+
+extension CurrentWeatherCell {
+    func setupConstraints() {
+        addSubview(temperature)
+        addSubview(weatherIcon)
+        addSubview(weatherDescription)
+        addSubview(feelsLike)
+        
+        // temperature constraints
+        temperature.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        temperature.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        // weatherIcon constraints
+        weatherIcon.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        weatherIcon.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        // Stack View
+        let stackView = UIStackView()
+        stackView.axis = NSLayoutConstraint.Axis.horizontal
+        stackView.distribution = UIStackView.Distribution.equalSpacing
+        stackView.alignment = UIStackView.Alignment.center
+        
+        stackView.addArrangedSubview(temperature)
+        stackView.addArrangedSubview(weatherIcon)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackView)
+        
+        
+        // Stack View constraints
+        stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+        stackView.widthAnchor.constraint(equalToConstant: 160).isActive = true
+//        stackView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+//
+//        // weatherDiscription constraints
+        weatherDescription.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 2).isActive = true
+//        weatherDescription.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        weatherDescription.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+//
+//
+//        // feelsLike constraints
+        feelsLike.topAnchor.constraint(equalTo: weatherDescription.bottomAnchor, constant: 8).isActive = true
+        feelsLike.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        
+    }
+}
--- Weather/Controller/WeatherViewController.swift
+++ Weather/Controller/WeatherViewController.swift
@@ -7,23 +7,88 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+let data = Bundle.main.decode(ModelWeather.self, from: "weather.json")
 
+class WeatherViewController: UIViewController {
+    
+    let sections: [ModelItem] = [
+        ModelItem(
+            data: data,
+            type: ModelType.currentWeather
+        ),
+    ]
+    var collectionView: UICollectionView!
+    
+    var dataSource: UICollectionViewDiffableDataSource<ModelItem, ModelWeather>?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .orange
+        setupCollectionView()
+        createDataSource()
+        reloadData()
+        
     }
-
-
+    
+    
+    func setupCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createCompositionalLayout())
+        
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .white
+        view.addSubview(collectionView)
+        collectionView.register(CurrentWeatherCell.self, forCellWithReuseIdentifier: CurrentWeatherCell.reuseId)
+    }
+    
+    // MARK: - Manage the data in UICV
+    
+    func createDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<ModelItem, ModelWeather>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, modelWeather) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CurrentWeatherCell.reuseId, for: indexPath) as? CurrentWeatherCell
+            cell?.configure(with: modelWeather)
+            return cell
+        })
+    }
+    
+    
+    func reloadData() {
+        var snapshot = NSDiffableDataSourceSnapshot<ModelItem, ModelWeather>()
+        snapshot.appendSections(sections)
+        
+        
+        for section in sections {
+            snapshot.appendItems([section.data], toSection: section)
+        }
+      
+        dataSource?.apply(snapshot)
+    }
+    
+    func createCompositionalLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            return self.createCurrentWeatherSection()
+        }
+        return layout
+    }
+    
+    
+    func createCurrentWeatherSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalWidth(1.0/2.0))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
+    }
+    
 }
-
-
-
-
-
-
-
 
 // MARK: - SwiftUI
 
