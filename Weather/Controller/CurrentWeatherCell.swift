c68afc3f2e2b1cc8429026dedcd9bae807cf7e2a
NosovaDaria <kuleshovadasha@gmail.com>
2022-12-04 18:06:48 +0300

    Adding SnapKit in CurrentWeatherCell

--- Weather/Controller/CurrentWeatherCell.swift
+++ Weather/Controller/CurrentWeatherCell.swift
@@ -5,103 +5,97 @@
 //  Created by Дарья Носова on 04.05.2022.
 //
 
-import Foundation
+import SnapKit
 import UIKit
 
 class CurrentWeatherCell: UICollectionViewCell {
     static var reuseId: String = "CurrentWeatherCell"
     
-    let temperature = UILabel()
-    
-    let weatherIcon = UIImageView()
-    let weatherDescription = UILabel()
-    let feelsLike = UILabel()
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor(white: 1, alpha: 1)
-        
-        customizeElements()
-        setupConstraints()
     }
     
     func configure(with modelWeather: ModelWeather) {
+        
         let weather = modelWeather.current
+        
+        let temperature = UILabel()
         temperature.text = "\(getTemperature(temperature: weather.temp))"
-        weatherIcon.image = UIImage(named: weather.weather[0].icon)
-        weatherDescription.text = weather.weather[0].description ?? ""
-        feelsLike.text = "Ощущается как \(getTemperature(temperature: weather.feels_like))"
-    }
-    
-    func customizeElements() {
         temperature.textColor = .black
         temperature.font = UIFont(name: "avenir", size: 40)
         temperature.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(temperature)
         
+        temperature.snp.makeConstraints { maker in
+            maker.width.equalTo(88)
+            maker.height.equalTo(88)
+        }
+        
+        let weatherIcon = UIImageView()
+        weatherIcon.image = UIImage(named: weather.weather[0].icon)
         weatherIcon.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(weatherIcon)
         
+        weatherIcon.snp.makeConstraints { maker in
+            maker.width.equalTo(88)
+            maker.width.equalTo(88)
+        }
+        
+        let stackView = UIStackView()
+        stackView.layer.shadowColor = UIColor.black.cgColor
+        stackView.layer.shadowOffset = CGSize(width: 5, height: 5)
+        stackView.layer.shadowOpacity = 0.2
+        stackView.layer.shadowRadius = 2.0
+        
+        stackView.axis = NSLayoutConstraint.Axis.horizontal
+        stackView.distribution = UIStackView.Distribution.equalCentering
+        stackView.alignment = UIStackView.Alignment.center
+        
+        stackView.addArrangedSubview(temperature)
+        stackView.addArrangedSubview(weatherIcon)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackView)
+        
+        stackView.snp.makeConstraints { maker in
+            maker.centerX.equalToSuperview()
+            maker.centerY.equalToSuperview()
+            maker.top.equalToSuperview().inset(10)
+            maker.width.equalTo(168)
+        }
+        
+        let weatherDescription = UILabel()
+        weatherDescription.text = weather.weather[0].description ?? ""
         weatherDescription.textColor = .black
         weatherDescription.font = UIFont(name: "avenir", size: 16)
         weatherDescription.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(weatherDescription)
         
+        weatherDescription.snp.makeConstraints { maker in
+            maker.top.equalTo(stackView).inset(150)
+            maker.centerX.equalToSuperview()
+        }
+        
+        let feelsLike = UILabel()
+        feelsLike.text = "Ощущается как \(getTemperature(temperature: weather.feels_like))"
         feelsLike.textColor = .black
         feelsLike.font = UIFont(name: "avenir", size: 16)
         feelsLike.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(feelsLike)
+        
+        feelsLike.snp.makeConstraints { maker in
+            maker.top.equalTo(weatherDescription).inset(40)
+            maker.centerX.equalToSuperview()
+        }
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
-// MARK: - Setup Constraints
 
 extension CurrentWeatherCell {
-    func setupConstraints() {
-        addSubview(temperature)
-        addSubview(weatherIcon)
-        addSubview(weatherDescription)
-        addSubview(feelsLike)
-        
-        // temperature constraints
-        temperature.widthAnchor.constraint(equalToConstant: 88).isActive = true
-        temperature.heightAnchor.constraint(equalToConstant: 80).isActive = true
-        
-        // weatherIcon constraints
-        weatherIcon.widthAnchor.constraint(equalToConstant: 80).isActive = true
-        weatherIcon.heightAnchor.constraint(equalToConstant: 80).isActive = true
-        
-        // StackView setups
-        let stackView = UIStackView()
-        stackView.axis = NSLayoutConstraint.Axis.horizontal
-        stackView.distribution = UIStackView.Distribution.equalCentering
-        stackView.alignment = UIStackView.Alignment.center
-    
-        //stackView's layer setups
-        stackView.layer.shadowColor = UIColor.black.cgColor
-        stackView.layer.shadowOffset = CGSize(width: 5, height: 5)
-        stackView.layer.shadowOpacity = 0.2
-        stackView.layer.shadowRadius = 2.0
-
-        stackView.addArrangedSubview(temperature)
-        stackView.addArrangedSubview(weatherIcon)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(stackView)
-        
-        
-        // Stack View constraints
-        stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        stackView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
-        stackView.widthAnchor.constraint(equalToConstant: 168).isActive = true
-        
-        // weatherDiscription constraints
-        weatherDescription.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 0).isActive = true
-        weatherDescription.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-
-        // feelsLike constraints
-        feelsLike.topAnchor.constraint(equalTo: weatherDescription.bottomAnchor, constant: 8).isActive = true
-        feelsLike.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-    }
     
     func getTemperature(temperature: Double) -> String {
         var temperatureWithSigns = ""
